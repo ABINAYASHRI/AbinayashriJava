f87cac1d9fcf82372fbf06384dd1443c8cbdccfc
Abi-A-02 <83580475+ABINAYASHRI@users.noreply.github.com>
2024-02-08 18:38:29 +0530

    Add files via upload

---
+++ JavaAssignment/StudentInfoSys/src/com/hexaware/controller/SisInterface.java
@@ -0,0 +1,31 @@
+package com.hexaware.controller;
+
+public interface SisInterface {
+	public void addStudent();
+
+	public void viewStudent();
+	
+	public void updateStudent();
+
+	public void deleteStudent();
+	
+//	public Student showStudentDetails();
+	
+	public void addTeacher();
+
+	public void viewTeacher();
+	
+	public void addCourse();
+
+	public void viewCourse();
+	
+	public void addEnrollment();
+
+	public void viewEnrollment();
+	
+	public void addPayment();
+
+	public void viewPayment();
+}
+
+
